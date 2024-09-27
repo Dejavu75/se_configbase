00e0c71d2e8a26592bc3321fff2a4ab80f423c47
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-09-26 23:57:01 -0300

    ts

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export declare function print(): void;
+export * from './src/schemas/sch_config';
+export type MySQLConfig2 = {
+    dee: string;
+};
---
+++ index.ts
@@ -0,0 +1,10 @@
+export function print() {
+    console.log('Hello, World!');
+  }
+
+ 
+  export * from './src/schemas/sch_config';
+
+  export type MySQLConfig2 = {
+    dee: string;
+  }
