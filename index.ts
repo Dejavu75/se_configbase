12bf6384388c7c28c3ad8c09dc3090f03696e7a1
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-10-09 21:40:23 -0300

    exports

--- index.ts
+++ index.ts
@@ -1,10 +1,6 @@
-import { mod_dataaccess } from './modules/mod_dataaccess';
 
 export * from './schemas/sch_config';
 export * from './controllers/conf_default_config';
-
-let oDA:mod_dataaccess = new mod_dataaccess('mt08','mt08');
-let oConfig =  oDA.controlarConfigBase().then(async (oConfig) => {
-    await oDA.controlarMSDB();
-    return oConfig;
-});
+export * from './modules/mod_dataaccess';
+export * from './controllers/con_dataaccess';
+export * from './controllers/con_log';
