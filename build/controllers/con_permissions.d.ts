8f9ff4f095c0b54f5ed4ca957da23a8db86ff98f
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-12-07 00:06:20 -0300

    Per2

--- build/controllers/con_permissions.d.ts
+++ build/controllers/con_permissions.d.ts
@@ -1,5 +1,8 @@
-import { cnt_Permission } from "se_contractholder";
-export declare class PermissionsController {
-    permissions: cnt_Permission[];
+import { cnt_Permission, cnt_SessionHolder, PermissionStatus, PermissionType } from "se_contractholder";
+export declare class PermissionsSet {
+    permissions: Map<string, cnt_Permission>;
+    domain: string;
     loadPermissions(): void;
+    createPermission(domainId: string, name: string, description?: string, domain?: string, status?: PermissionStatus, type?: PermissionType): cnt_Permission;
+    checkPermission(domainId: string, session: cnt_SessionHolder): boolean;
 }
--- build/controllers/con_permissions.js
+++ build/controllers/con_permissions.js
@@ -1,11 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PermissionsController = void 0;
-class PermissionsController {
+exports.PermissionsSet = void 0;
+const se_contractholder_1 = require("se_contractholder");
+class PermissionsSet {
     constructor() {
-        this.permissions = [];
+        this.permissions = new Map();
+        this.domain = "global";
     }
     loadPermissions() {
     }
+    createPermission(domainId, name, description = "", domain = "global", status = se_contractholder_1.PermissionStatus.enabled, type = se_contractholder_1.PermissionType.permissive) {
+        let permiso = new se_contractholder_1.cnt_Permission(0, domainId, name, description, status, type, domain);
+        this.permissions.set(permiso.domainId, permiso);
+        return permiso;
+    }
+    checkPermission(domainId, session) {
+        let permiso = this.permissions.get(domainId);
+        if (permiso) {
+            let found = false;
+            session.accountHolder.permissions.forEach((p) => {
+                if ((p.status == se_contractholder_1.PermissionStatus.enabled || (p.status == se_contractholder_1.PermissionStatus.default && permiso.status == se_contractholder_1.PermissionStatus.enabled)) &&
+                    p.domainId === permiso.domainId &&
+                    (p.domain == session.domain || p.domain == "global" || session.domain == "global")) {
+                    found = true;
+                }
+                else {
+                    found = found || false;
+                }
+            });
+            return found;
+        }
+        else {
+            return false;
+        }
+    }
 }
-exports.PermissionsController = PermissionsController;
+exports.PermissionsSet = PermissionsSet;
