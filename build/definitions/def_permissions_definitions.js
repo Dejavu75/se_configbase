0abc6a700cd1d4e9067f953610f53484229d15e8
Dejavu75 <diego.zacur@dvsoft.com.ar>
2025-01-05 23:11:46 -0300

    def

--- build/definitions/def_permissions_definitions.js
+++ build/definitions/def_permissions_definitions.js
@@ -11,6 +11,10 @@ class CH09_AEM_Permissions extends con_permissions_1.PermissionsSet {
         this.createPermission('productsCreate', 'Create', 'Create a new product', 'foreign');
         this.createPermission('productsDelete', 'Delete', 'Delete products', 'foreign');
         this.createPermission('productsUpdate', 'Update', 'Update products', 'foreign');
+        this.createPermission('bultsInfo', 'Info', 'Get info about a bult', 'foreign');
+        this.createPermission('bultsCreate', 'Create', 'Create a new bult', 'foreign');
+        this.createPermission('bultsDelete', 'Delete', 'Delete bult', 'foreign');
+        this.createPermission('bultsUpdate', 'Update', 'Update bult', 'foreign');
     }
 }
 exports.CH09_AEM_Permissions = CH09_AEM_Permissions;
--- definitions/def_permissions_definitions.ts
+++ definitions/def_permissions_definitions.ts
@@ -9,5 +9,11 @@ export class CH09_AEM_Permissions extends PermissionsSet {
       this.createPermission('productsCreate', 'Create', 'Create a new product', 'foreign')
       this.createPermission('productsDelete', 'Delete', 'Delete products', 'foreign')
       this.createPermission('productsUpdate', 'Update', 'Update products', 'foreign')
+
+      this.createPermission('bultsInfo', 'Info', 'Get info about a bult', 'foreign')
+      this.createPermission('bultsCreate', 'Create', 'Create a new bult', 'foreign')
+      this.createPermission('bultsDelete', 'Delete', 'Delete bult', 'foreign')
+      this.createPermission('bultsUpdate', 'Update', 'Update bult', 'foreign')
+      
     }
   }
