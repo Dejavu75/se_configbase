869a97aa5112e1c3bc31433b0b767a7403ed5d08
Dejavu75 <diego.zacur@dvsoft.com.ar>
2025-01-06 00:48:39 -0300

    tsc

--- build/definitions/def_permissions_definitions.d.ts
+++ build/definitions/def_permissions_definitions.d.ts
@@ -1,5 +1,17 @@
+import { cnt_Permission, cnt_SessionHolder } from "se_contractholder";
 import { PermissionsSet } from "../controllers/con_permissions";
+export declare enum CH09_EnumP {
+    productsCreate = "productsCreate",
+    productsDelete = "productsDelete",
+    productsUpdate = "productsUpdate",
+    palletsInfo = "palletsInfo",
+    palletsCreate = "palletsCreate",
+    palletsDelete = "palletsDelete",
+    palletsUpdate = "palletsUpdate"
+}
 export declare class CH09_AEM_Permissions extends PermissionsSet {
     constructor();
+    createlocalPermission(domainId: CH09_EnumP, name: string, description?: string, domain?: string): cnt_Permission;
+    checklocalPermission(domainId: CH09_EnumP, session: cnt_SessionHolder): boolean;
     loadPermissions(): void;
 }
--- build/definitions/def_permissions_definitions.js
+++ build/definitions/def_permissions_definitions.js
@@ -1,20 +1,36 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CH09_AEM_Permissions = void 0;
+exports.CH09_AEM_Permissions = exports.CH09_EnumP = void 0;
 const con_permissions_1 = require("../controllers/con_permissions");
+var CH09_EnumP;
+(function (CH09_EnumP) {
+    CH09_EnumP["productsCreate"] = "productsCreate";
+    CH09_EnumP["productsDelete"] = "productsDelete";
+    CH09_EnumP["productsUpdate"] = "productsUpdate";
+    CH09_EnumP["palletsInfo"] = "palletsInfo";
+    CH09_EnumP["palletsCreate"] = "palletsCreate";
+    CH09_EnumP["palletsDelete"] = "palletsDelete";
+    CH09_EnumP["palletsUpdate"] = "palletsUpdate";
+})(CH09_EnumP || (exports.CH09_EnumP = CH09_EnumP = {}));
 class CH09_AEM_Permissions extends con_permissions_1.PermissionsSet {
     constructor() {
         super();
         this.loadPermissions();
     }
+    createlocalPermission(domainId, name, description, domain) {
+        return this.createPermission(domainId.toString(), name, description, domain);
+    }
+    checklocalPermission(domainId, session) {
+        return this.checkPermission(domainId.toString(), session);
+    }
     loadPermissions() {
-        this.createPermission('productsCreate', 'Create', 'Create a new product', 'foreign');
-        this.createPermission('productsDelete', 'Delete', 'Delete products', 'foreign');
-        this.createPermission('productsUpdate', 'Update', 'Update products', 'foreign');
-        this.createPermission('bultsInfo', 'Info', 'Get info about a bult', 'foreign');
-        this.createPermission('bultsCreate', 'Create', 'Create a new bult', 'foreign');
-        this.createPermission('bultsDelete', 'Delete', 'Delete bult', 'foreign');
-        this.createPermission('bultsUpdate', 'Update', 'Update bult', 'foreign');
+        this.createlocalPermission(CH09_EnumP.productsCreate, 'Create', 'Create a new product', 'foreign');
+        this.createlocalPermission(CH09_EnumP.productsDelete, 'Delete', 'Delete products', 'foreign');
+        this.createlocalPermission(CH09_EnumP.productsUpdate, 'Update', 'Update products', 'foreign');
+        this.createlocalPermission(CH09_EnumP.palletsInfo, 'Info', 'Get info about a bult', 'foreign');
+        this.createlocalPermission(CH09_EnumP.productsCreate, 'Create', 'Create a new bult', 'foreign');
+        this.createlocalPermission(CH09_EnumP.productsDelete, 'Delete', 'Delete bult', 'foreign');
+        this.createlocalPermission(CH09_EnumP.productsUpdate, 'Update', 'Update bult', 'foreign');
     }
 }
 exports.CH09_AEM_Permissions = CH09_AEM_Permissions;
